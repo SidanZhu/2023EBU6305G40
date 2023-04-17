b6f5e135b6dc74403b0fff7ba40a6267582a2b52
吉睿舒 <106053648+eatjuzi@users.noreply.github.com>
2023-04-17 22:23:39 +0800

    Update prospect.js

--- version3/assets/js/prospect.js
+++ version3/assets/js/prospect.js
@@ -344,7 +344,7 @@ option6 = {
                 }
             },
             axisLabel: {
-                fontSize: 30,
+                fontSize: 27,
                 distance: 25,
                 formatter: function (value) {
                     if (value === 0) {
